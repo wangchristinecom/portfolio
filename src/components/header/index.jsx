eb9baaf1e26c7420f99fadef7e004f463c146696
typeawoman <mail@typeawoman.com>
2021-04-18 23:15:18 +0200

    twitter delete

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -40,17 +40,6 @@ const Header = ({ metadata = {}, noBlog = false }) => {
         </h1>
         <p className={classes.description}>{metadata.description}</p>
         <ul className={classes.list}>
-           {
-           twitter && (
-            <li className={classes.item}>
-              <a
-                className={classes.link}
-                href={`https://twitter.com/${twitter}`}
-              >
-                Twitter
-              </a>
-            </li>
-          )}
           {github && (
             <li className={classes.item}>
               <a className={classes.link} href={github}>
